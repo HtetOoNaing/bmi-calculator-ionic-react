0a31ac7b4f3b6c04f9157cb8064dd2bce65ae8f2
Htet Oo Naing <htetoonaing.ycc@gmail.com>
2021-07-19 23:12:34 +0630

    layout design

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import { IonApp, IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/react';
+import { IonApp, IonButton, IonCol, IonContent, IonGrid, IonHeader, IonIcon, IonInput, IonItem, IonLabel, IonRow, IonTitle, IonToolbar } from '@ionic/react';
+import { calculatorOutline, refreshOutline } from 'ionicons/icons';
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
@@ -26,8 +27,40 @@ const App: React.FC = () => (
 				<IonTitle>BMI Calculator</IonTitle>
 			</IonToolbar>
 		</IonHeader>
-    <IonContent>
-			
+    <IonContent className="ion-padding">
+			<IonGrid>
+				<IonRow>
+					<IonCol>
+						<IonItem>
+							<IonLabel position="floating">Your Height</IonLabel>
+							<IonInput></IonInput>
+						</IonItem>
+					</IonCol>
+				</IonRow>
+				<IonRow>
+					<IonCol>
+						<IonItem>
+							<IonLabel position="floating">Your Weight</IonLabel>
+							<IonInput></IonInput>
+						</IonItem>
+					</IonCol>
+				</IonRow>
+				<IonRow>
+					<IonCol className="ion-text-left">
+						<IonButton>
+							<IonIcon icon={calculatorOutline} slot="start"  /> Calculate
+						</IonButton>
+					</IonCol>
+					<IonCol className="ion-text-right">
+						<IonButton>
+							<IonIcon icon={refreshOutline} slot="start"  /> Reset
+						</IonButton>
+					</IonCol>
+				</IonRow>
+				<IonRow>
+					<IonCol></IonCol>
+				</IonRow>
+			</IonGrid>
 		</IonContent>
   </IonApp>
 );
